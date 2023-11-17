89e46d610e2a2b7e350295fae7076de961194f3b
KotoPumpkin <736623136@qq.com>
2023-11-17 18:33:05 +0800

    class 3 assignment

--- 1.go
+++ 1.go
@@ -2,7 +2,58 @@ package main
 
 import "fmt"
 
+type Item struct {
+	Name  string
+	Price float64
+	Stock int
+}
+
+type StockManager interface {
+	CheckStock() int
+	UpdateStock(quantity int)
+	PrintStockInfo()
+}
+
+type Electronics struct {
+	Item
+	Brand string
+	Model string
+}
+
+func (e *Electronics) CheckStock() int {
+	return e.Stock
+}
+
+func (e *Electronics) UpdateStock(quantity int) {
+	e.Stock += quantity
+}
+
+func (e *Electronics) PrintStockInfo() {
+	fmt.Printf("Brand: %s, Model: %s\n", e.Brand, e.Model)
+	fmt.Printf("Stock: %d\n", e.Stock)
+}
+
 func main() {
-	var a [5]int
-	fmt.Print(a)
+	iphone := &Electronics{
+		Item: Item{
+			Name:  "羽中的亲妈",
+			Price: 999.99,
+			Stock: 10,
+		},
+		Brand: "雷蒙",
+		Model: "雷蒙太太",
+	}
+
+	fmt.Println("Initial Stock:")
+	iphone.PrintStockInfo()
+
+	fmt.Println("Checking stock...")
+	quantity := iphone.CheckStock()
+	fmt.Printf("Current stock: %d\n", quantity)
+
+	fmt.Println("Updating stock...")
+	iphone.UpdateStock(5)
+
+	fmt.Println("Updated Stock:")
+	iphone.PrintStockInfo()
 }
---
+++ hitori/bocchi.go
@@ -0,0 +1,22 @@
+package hitori
+
+func Bocchi(a, b int) int {
+	var c int
+	c = a + b
+	return c
+}
+func Kita(a, b int) int {
+	var c int
+	c = a - b
+	return c
+}
+func Ryo(a, b int) int {
+	var c int
+	c = a * b
+	return c
+}
+func Nijika(a, b int) int {
+	var c int
+	c = a / b
+	return c
+}
